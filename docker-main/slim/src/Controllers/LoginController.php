06d33f7902690a3758d51667b8696d25095b837d
Tiago <elrulaso.mp3@gmail.com>
2026-04-13 20:52:37 -0300

    avances

--- docker-main/slim/src/Controllers/LoginController.php
+++ docker-main/slim/src/Controllers/LoginController.php
@@ -2,7 +2,7 @@
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-
+require __DIR__ . '/../Models/DB.php';
 class loginController {
 
 public function login(Request $request, Response $response, array $args){
@@ -19,14 +19,18 @@ public function login(Request $request, Response $response, array $args){
         $password = $datos['password'];
         //Si el email y la contraseña coinciden con un usuario existente envio un OK
         //Esto se compara asi porque no hay base de datos para comparar
-        if($email === 'ejemplo@gmail' && $password === 'ejemplo'){
+        $db = DB::getConnection();
+        $prueba =  $db->query("SELECT email, password FROM users WHERE email = '$email' AND password = '$password'")->fetch(PDO::FETCH_ASSOC);
+        if($prueba['email'] == $email && $prueba['password'] == $password){ 
             $cumple = ["status" => "200 OK", "message" => "Se completo existosamente el login"];    //<-creo el mensaje
-            $response->getBody()->write(json_encode($cumple));      
+            $response->getBody()->write(json_encode($cumple));  
+
             return $response->withHeader("Content-Type", "application/json")->withStatus(200);      //<-envio el json con el OK
         } else {
             //Si no cumplen con un usuario existente envio un Bad request
             $error = ["Status" => "400 Bad request", "message" => "El email o contraseña no coinciden con un usuario existente"];       //<-creo el mensaje
             $response->getBody()->write(json_encode($error));       
+
             return $response->withHeader("Content-Type", "application/json")->withStatus(400);      //<-envio el json con el Bad request
         }
     }
--- docker-main/slim/src/Routes.php
+++ docker-main/slim/src/Routes.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once __DIR__ . '/Controllers/LoginController.php';
-
+//login
 $app->post('/login', [LoginController::class ,'login']);
