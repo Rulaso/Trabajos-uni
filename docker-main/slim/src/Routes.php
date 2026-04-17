300e6786ad721771e3cac0a81c2281bffd387edb
Rulaso <elrulaso.mp3@gmail.com>
2026-04-17 20:02:19 -0300

    saque el trabajo de php

--- docker-main/slim/src/Routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/Controllers/LoginController.php';
-require_once __DIR__ .'';
-//login
-$app->post('/login', [LoginController::class ,'login']);
-//logout
-$app->post('/logout', [LoginController::class ,'logout']);
